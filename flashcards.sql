3eab2b8a4b910028818caebc1e3eaad13b1b0eea
Stephen Chung <stephen@usesilo.com>
2024-07-08 21:46:35 -0400

    imit

---
+++ flashcards.sql
@@ -0,0 +1,11 @@
+create table flashcards (
+	id SERIAL PRIMARY KEY,
+	prompt TEXT NOT NULL,
+	answer TEXT -- this can be null in case you're still researching the answer
+);
+
+create table flashcard_tags (
+	id SERIAL PRIMARY KEY,
+	flashcard_id INTEGER NOT NULL,
+	tag TEXT NOT NULL
+);
