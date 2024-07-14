2db83f9e345389ed322bfaa0712daf33986fe039
Stephen Chung <spchung95@gmail.com>
2024-07-14 18:36:34 -0400

    updateable answers

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -20,6 +21,23 @@ func main() {
 		fmt.Fprintf(os.Stderr, "failed to connect to postgres: %v\n", err)
 	}
 	defer conn.Close(context.Background())
+	http.HandleFunc("/flashcards/{id}", func(w http.ResponseWriter, r *http.Request) {
+		answer := r.FormValue("answer")
+		id, err := strconv.Atoi(r.PathValue("id"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "internal server error: %s", err)
+			return
+		}
+
+		tag, err := conn.Exec(context.Background(), `UPDATE flashcards SET answer = $1 WHERE id = $2`, answer, id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "internal server error: %s", err)
+			return
+		}
+		w.Write([]byte(fmt.Sprintf("inserted rows: %d", tag.RowsAffected())))
+	})
 	http.HandleFunc("/flashcards", func(w http.ResponseWriter, r *http.Request) {
 		prompt := r.FormValue("prompt")
 		answer := r.FormValue("answer")
@@ -74,6 +92,8 @@ func main() {
 					margin: 0 2rem;
 				}
 				.flashcard {
+					display: flex;
+					gap: 4rem;
 				}
 				.hidden {
 					display: none;
@@ -94,13 +114,13 @@ func main() {
 				  {{ range $flashcard := . }}
 				  	<div class="flashcard">
 						<div>{{ .Prompt }}</div>
-						{{ if .Answer }}
-							<div id="flashcard-{{ .ID }}" class="hidden">
-								{{ .Answer }}
-							</div>
-							<button hx-on:click="document.getElementById('flashcard-{{.ID}}').classList.toggle('hidden')">show/hide</button>
-						{{end}}
-
+						<button hx-on:click="document.getElementById('flashcard-{{.ID}}').classList.toggle('hidden')">show/hide</button>
+						<div id="flashcard-{{ .ID }}" class="flashcard-answer hidden">
+							<form hx-patch="/flashcards/{{.ID}}">
+								Answer: <input type="text" name="answer" value="{{ .Answer }}"></input>
+								<button type="submit">update</button>
+							</form>
+						</div>
 					</div>
 				  {{ end }}
 				</u>
