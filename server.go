f43b8ebce5f668d524f0b3f59d044f958053a6a6
Stephen Chung <spchung95@gmail.com>
2024-07-15 23:23:33 -0400

    templatize

--- server.go
+++ server.go
@@ -18,8 +18,10 @@ var (
 	PORT         = cmp.Or(os.Getenv("PORT"), ":8080")
 	DATABASE_URL = cmp.Or(os.Getenv("DATABASE_URL"), "postgres://localhost:5432/flashcards")
 
-	//go:embed assets/*
+	//go:embed assets
 	assets embed.FS
+	//go:embed views
+	views embed.FS
 )
 
 type Flashcard struct {
@@ -74,19 +76,7 @@ func main() {
 			return
 		}
 
-		t, _ := template.New("flashcard-frag").Parse(`
-				<div class="flashcard">
-					<div class="flashcard-prompt">
-						<div>{{ .Prompt }}</div>
-					</div>
-					<div id="flashcard-{{ .ID }}" class="flashcard-answer">
-						<form id="update-flashcard-{{.ID}}-answer" hx-patch="/flashcards/{{.ID}}" hx-swap="none" class="hidden">
-							Answer: <input type="text" name="answer" value="{{ .Answer }}"></input>
-							<button type="submit">update</button>
-							</form>
-						<button hx-on:click="document.getElementById('update-flashcard-{{.ID}}-answer').classList.toggle('hidden')">👀</button>
-					</div>
-				</div>`)
+		t, _ := template.ParseFS(views, "flashcard")
 		t.Execute(w, created)
 	})
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
@@ -110,53 +100,19 @@ func main() {
 
 		// interpolate the flashcards in a list
 
-		t, err := template.New("homepage").Parse(`
-		<!DOCTYPE html>
-		<html>
-		  <head>
-		  	<script src="https://unpkg.com/htmx.org@2.0.1" integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/" crossorigin="anonymous"></script>
-		  	<link rel="stylesheet" href="/assets/root.css">
-		  </head>
-          <body>
-		    <h1>Flashcards</h1>
-			<div class="flashcard">
-				<form hx-post="/flashcards" hx-swap="afterbegin" hx-target=".flashcard-container" hx-on::after-request="this.reset()">
-					<label for="prompt">prompt:</label>
-					<input id="prompt" name="prompt" type="text" />
-					<label for="answer">answer:</label>
-					<input id="answer" name="answer" type="text"/>
-					<button>create</button>
-				</form>
-			</div>
-			<div class="flashcard-container">
-				{{ range $flashcard := . }}
-				<div class="flashcard">
-					<div class="flashcard-prompt">
-						<div>{{ .Prompt }}</div>
-						<details>
-							<summary>answer</summary>
-							<p>{{.Answer}}</p>
-						</details>
-					</div>
-					<div id="flashcard-{{ .ID }}" class="flashcard-answer">
-						<form id="update-flashcard-{{.ID}}-answer" hx-patch="/flashcards/{{.ID}}" hx-swap="none" class="hidden">
-							Answer: <input type="text" name="answer" value="{{ .Answer }}"></input>
-							<button type="submit">update</button>
-							</form>
-						<button hx-on:click="document.getElementById('update-flashcard-{{.ID}}-answer').classList.toggle('hidden')">✏️</button>
-					</div>
-				</div>
-				{{ end }}
-			</div>
-		  </body>
-		</html>
-		`)
+		tmpls, err := template.ParseFS(views, "views/root.html", "views/flashcard.html")
+		fmt.Println(tmpls)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprintf(w, "internal server error: %s", err)
 			return
 		}
-		t.Execute(w, flashcards)
+		tmpls.ExecuteTemplate(w, "root", flashcards)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "internal server error: %s", err)
+			return
+		}
 	})
 	if err := http.ListenAndServe(PORT, nil); err != nil {
 		log.Fatalf("server failed: %v", err)
