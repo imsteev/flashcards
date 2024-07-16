6fcb1cffa146aa07e7212c534f081525cc4d576c
Stephen Chung <spchung95@gmail.com>
2024-07-15 22:36:51 -0400

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# flashcards
+
+## development
+For first time setup, you will need to initialize tables. If you use `psql`, you can shell in to execute `install.sql`.
+```
+flashcards=# \i install.sql;
+```
--- install.sql
+++ install.sql
@@ -1,10 +1,10 @@
-create table flashcards (
+create table if not exists flashcards (
 	id SERIAL PRIMARY KEY,
 	prompt TEXT NOT NULL,
 	answer TEXT -- this can be null in case you're still researching the answer
 );
 
-create table flashcard_tags (
+create table if not exists flashcard_tags (
 	id SERIAL PRIMARY KEY,
 	flashcard_id INTEGER NOT NULL,
 	tag VARCHAR(100) NOT NULL UNIQUE
